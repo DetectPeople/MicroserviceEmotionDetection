98b33d621c140d0fa7d570b0538266fd62d7f2df
fiorella valencia <fiorella.valencia.oh@gmail.com>
2024-11-12 02:02:47 -0500

    Update process_emotion_use_case.py

--- use_cases/process_emotion_use_case.py
+++ use_cases/process_emotion_use_case.py
@@ -11,6 +11,7 @@ from entities import db
 from entities.emotion import Emotion
 from config import Config
 import os
+import numpy as np
 
 class ProcessEmotionUseCase:
     def __init__(self):
@@ -36,15 +37,18 @@ class ProcessEmotionUseCase:
             if response.status_code != 200:
                 raise ValueError(f"Error generating frames: {response.text}")
 
-            from entities import db
-
             # Retry to get frames after the generation request
             max_retries = 10
             wait_time = 2
+            timeout = 30  # Timeout to avoid waiting indefinitely
+            start_time = time.time()
             for attempt in range(max_retries):
+                if time.time() - start_time > timeout:
+                    logging.error("Timeout reached while waiting for frames.")
+                    return None
+
                 time.sleep(wait_time)
                 db.session.expire_all()
-                db.session.commit()
                 frames = self.frame_repository.get_frames_by_video_id(video_id)
                 if frames:
                     logging.info(f"Frames found on attempt {attempt + 1}")
@@ -61,57 +65,56 @@ class ProcessEmotionUseCase:
         if existing_emotions:
             self.emotion_repository.delete_emotions_by_video_id(video_id)
 
-            # Process frames and annotate them
-            annotated_frames = []
-            total_frames = len(frames)
-            for idx, frame in enumerate(frames):
-                annotations, annotated_frame_path, frame_data = self.emotion_service.detect_emotions_in_frame(
-                    frame.path)
-                frame_data['annotated_path'] = annotated_frame_path
-                frame_data['frame_id'] = frame.id
-                annotated_frames.append(frame_data)
+        # Process frames and annotate them
+        annotated_frames = []
+        frames_to_update = []
+        persons_to_update = []
+        total_frames = len(frames)
+        for idx, frame in enumerate(frames):
+            annotations, annotated_frame_path, frame_data = self.emotion_service.detect_emotions_in_frame(frame.path)
+            frame_data['annotated_path'] = annotated_frame_path
+            frame_data['frame_id'] = frame.id
+            annotated_frames.append(frame_data)
 
-                # Update frame with frame_data
-                frame.average_brightness = frame_data['average_brightness']
-                frame.contrast_ratio = frame_data['contrast_ratio']
-                frame.sharpness = frame_data['sharpness']
-                frame.color_temperature = frame_data['color_temperature']
-                frame.background_complexity = frame_data['background_complexity']
+            # Update frame with frame_data
+            frame.average_brightness = float(frame_data['average_brightness']) if isinstance(frame_data['average_brightness'], np.float32) else frame_data['average_brightness']
+            frame.contrast_ratio = float(frame_data['contrast_ratio']) if isinstance(frame_data['contrast_ratio'], np.float32) else frame_data['contrast_ratio']
+            frame.sharpness = float(frame_data['sharpness']) if isinstance(frame_data['sharpness'], np.float32) else frame_data['sharpness']
+            frame.color_temperature = float(frame_data['color_temperature']) if isinstance(frame_data['color_temperature'], np.float32) else frame_data['color_temperature']
+            frame.background_complexity = float(frame_data['background_complexity']) if isinstance(frame_data['background_complexity'], np.float32) else frame_data['background_complexity']
+            frames_to_update.append(frame)
 
-                db.session.add(frame)  # Mark frame as modified
+            for annotation in annotations:
+                bbox_str = ",".join(map(str, annotation['bbox']))
+                person = self.person_repository.get_or_create(video_id, annotation['person_identity'])
 
-                for annotation in annotations:
-                    bbox_str = ",".join(map(str, annotation['bbox']))
-                    person = self.person_repository.get_or_create(video_id, annotation['person_identity'])
+                # Update person with age and gender estimation
+                person.age_estimation = float(annotation['age_estimation']) if isinstance(annotation['age_estimation'], np.float32) else annotation['age_estimation']
+                person.gender_estimation = annotation['gender_estimation']
+                persons_to_update.append(person)
 
-                    # Update person with age and gender estimation
-                    person.age_estimation = annotation['age_estimation']
-                    person.gender_estimation = annotation['gender_estimation']
-                    db.session.add(person)
+                emotion = Emotion(
+                    frame_id=frame.id,
+                    person_id=person.id,
+                    emotion_type=annotation['emotion'],
+                    confidence=annotation['confidence'],
+                    bbox=bbox_str,
+                    video_id=video.id
+                )
+                self.emotion_repository.save(emotion)
 
-                    emotion = Emotion(
-                        frame_id=frame.id,
-                        person_id=person.id,
-                        emotion_type=annotation['emotion'],
-                        confidence=annotation['confidence'],
-                        bbox=bbox_str,
-                        video_id=video.id
-                    )
-                    self.emotion_repository.save(emotion)
+            progress_percent = int(((idx + 1) / total_frames) * 100)
+            logging.info(f"Processed frame {idx + 1}/{total_frames} ({progress_percent}% complete)")
 
-                progress_percent = int(((idx + 1) / total_frames) * 100)
-                logging.info(f"Processed frame {idx + 1}/{total_frames} ({progress_percent}% complete)")
+        # Commit all changes to frames and persons
+        db.session.add_all(frames_to_update)
+        db.session.add_all(persons_to_update)
+        db.session.commit()
 
-            # Commit all changes to frames and persons
-            db.session.commit()
+        # Generate annotated video
+        output_video_filename = f'video_{video.id}_emotion_annotated.mp4'
+        output_video_path = os.path.join(Config.ANNOTATED_VIDEOS_DIR, output_video_filename)
+        self.video_generation_service.generate_video_from_frames(annotated_frames, output_video_path)
 
-            # Generate annotated video
-            output_video_filename = f'video_{video.id}_emotion_annotated.mp4'
-            output_video_path = self.video_generation_service.generate_video_from_frames(
-                annotated_frames, output_video_filename
-            )
-
-            logging.info("Emotion detection complete, annotated video generated.")
-            return output_video_path
-
-
+        logging.info("Emotion detection complete, annotated video generated.")
+        return output_video_path
